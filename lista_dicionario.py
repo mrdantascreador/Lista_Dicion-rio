c1fa9fea90b4115e4d144902a0b10a2da3b1463e
mrdantascreador <kaiocesar9977@gmail.com>
2025-09-05 13:59:05 +0000

    atividade de lista

---
+++ lista_dicionario.py
@@ -0,0 +1,26 @@
+frutas = ['Abacaxi' , 'Banana' , 'Cereja', 'Morango', 'Maça']
+print(frutas[0])
+print(frutas[-1])
+print(frutas)
+
+numeros = [1,2,3,4,5]
+numeros.append(6)
+numeros.insert(0,0)
+numeros.remove(3)  
+print(numeros)
+
+
+lista = [1,2,3]
+lista2 = [4,5,6]
+juncao = lista + lista2
+print(juncao)
+
+letras = ['b', 'd', 'a', 'c' , 'e']
+letras.sort()
+print(letras)
+letras.reverse()
+print(letras)
+print(letras.count('a'))
+print(letras)
+letras.pop()
+print(letras)
